79078d2a127f0aed5e38d6c4abb6546e9e1ad01b
chentutu <1694089327@qq.com>
2017-11-07 16:20:03 +0800

    mapper.xml文件上上相关修改

--- src/com/beijiao/serviceImpl/AdminServiceImpl.java
+++ src/com/beijiao/serviceImpl/AdminServiceImpl.java
@@ -3,8 +3,14 @@
  */
 package com.beijiao.serviceImpl;
 
+import java.util.Map;
+
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Service;
 
+import com.beijiao.dao.AdminDao;
+import com.beijiao.model.Admin;
 import com.beijiao.service.AdminService;
 
 /**
@@ -13,5 +19,21 @@ import com.beijiao.service.AdminService;
  */
 @Service("adminService")
 public class AdminServiceImpl implements AdminService {
+
+	@Resource
+	private AdminDao adminDao;
+	
+	
+	@Override
+	public Admin adminLogin(Map map) {
+		// TODO Auto-generated method stub
+		return adminDao.adminLogin(map);
+	}
+
+	@Override
+	public int changeAdminPswd(String password) {
+		// TODO Auto-generated method stub
+		return adminDao.changeAdminPswd(password);
+	}
 
 }
